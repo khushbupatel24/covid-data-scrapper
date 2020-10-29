6acef91346d74c25950219fc55a46e567fa36a2d
Khushbu <pkhush1993@gmail.com>
2020-10-29 22:18:00 +0530

    Done - store data in github JSON file from scrapper

--- scrapper.py
+++ scrapper.py
@@ -1,3 +1,5 @@
+import requests
+import base64
 import csv
 import json
 import pandas as pd
@@ -7,20 +9,40 @@ from  urllib.request  import urlretrieve as retrieve
 
 def filter():
     col_list = ["prname", "pruid","date","numconf","numdeaths","numtotal","numtested","numrecover","percentrecover","numtoday","percentoday","ratetotal","ratedeaths","percentactive","numdeathstoday","percentdeath","percentactive"]
-    
+
     yesterday = date.today() - timedelta(days=1)
     print("Date:", yesterday)
 
     df = pd.read_csv("https://health-infobase.canada.ca/src/data/covidLive/covid19-download.csv",usecols=col_list)
     df['date_filter'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
     df_filtered = df[(df['date_filter'] == yesterday.strftime('%Y-%m-%d'))]
-    #df_filtered.to_csv("covidData.csv")
+
     df_filtered.to_json (r'covidData.json')
 
-filter()
+    data = open("covidData.json", "r").read()
 
-# def j():
-   
-#     df = pd.read_csv('covidData.csv')
-#     df.to_json (r'a.json')
-# j()
+    token = "385b412e10720f3ea07bef43cf810bde19e1d828"
+    owner = 'khushbupatel24'
+    repo = 'covid-data'
+    path = 'deathRate.json'
+
+    url = 'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
+    r = requests.get(f'https://api.github.com/repos/{owner}/{repo}/contents/{path}')
+    print(r.json()['sha'])
+
+    r = requests.put(
+        f'https://api.github.com/repos/{owner}/{repo}/contents/{path}',
+        headers = {
+            'Authorization': f'Token {token}'
+        },
+        json = {
+            "message": "add new file",
+            "content": base64.b64encode(data.encode()).decode(),
+            "branch": "main",
+            "sha": r.json()['sha']
+        }
+    )
+    print(r.status_code)
+    print(r.json())
+
+filter()
