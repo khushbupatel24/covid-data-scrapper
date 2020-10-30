346ee2bcff9ffcaa424a9961a2a79560548260b4
Khushbu <pkhush1993@gmail.com>
2020-10-31 00:56:48 +0530

    Fix - add new file on git if not exists fixed

--- scrapper.py
+++ scrapper.py
@@ -7,6 +7,47 @@ from datetime import date, timedelta
 import datetime
 from  urllib.request  import urlretrieve as retrieve
 
+def uploadDataOnGit(file):
+    token = "385b412e10720f3ea07bef43cf810bde19e1d828"
+    owner = 'khushbupatel24'
+    repo = 'covid-data'
+
+    data = open(file, "r").read()
+    path = 'data/' + file
+
+    url = 'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
+    r = requests.get(f'https://api.github.com/repos/{owner}/{repo}/contents/{path}')
+    print(r.status_code)
+
+    if r.status_code != 200 :
+        r = requests.put(
+            f'https://api.github.com/repos/{owner}/{repo}/contents/{path}',
+            headers = {
+                'Authorization': f'Token {token}'
+            },
+            json = {
+                "message": "add new file",
+                "content": base64.b64encode(data.encode()).decode(),
+                "branch": "main",
+            }
+        )
+        print(r.json())
+    else:
+        r = requests.put(
+            f'https://api.github.com/repos/{owner}/{repo}/contents/{path}',
+            headers = {
+                'Authorization': f'Token {token}'
+            },
+            json = {
+                "message": "add new file",
+                "content": base64.b64encode(data.encode()).decode(),
+                "branch": "main",
+                "sha": r.json()['sha']
+            }
+        )
+        print(r.status_code)
+
+
 def filter():
     col_list = ["prname", "pruid","date","numconf","numdeaths","numtotal","numtested","numrecover","percentrecover","numtoday","percentoday","ratetotal","ratedeaths","percentactive","numdeathstoday","percentdeath","percentactive"]
     col1=["prname","numdeaths"] # columns for death chart
@@ -20,59 +61,36 @@ def filter():
     df = pd.read_csv("https://health-infobase.canada.ca/src/data/covidLive/covid19-download.csv",usecols=col_list)
     df['date_filter'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
     df_filtered = df[(df['date_filter'] == yesterday.strftime('%Y-%m-%d'))]
-    
+
     d=df_filtered.filter(col1) #filter by columns
     d=d.rename(columns={"prname": "province", "numdeaths": "number of deaths"}) #renaming columns
     d=d.set_index("province") #changing keys
-    d.to_json (r'Death.json')
+    d.to_json (r'deathRate.json')
+    uploadDataOnGit('deathRate.json')
 
     d=df_filtered.filter(col2)
     d=d.rename(columns={"prname": "province", "numtested": "people tested"})
     d=d.set_index("province")
     d.to_json (r'Tested.json') # file for number of people tested 
+    uploadDataOnGit('Tested.json')
 
-    
     d=df_filtered.filter(col3)
     d=d.rename(columns={"prname": "province", "numrecover": "recovered"})
     d=d.set_index("province")
     d.to_json (r'recovered.json') # file for total recovered cases
+    uploadDataOnGit('recovered.json')
 
     d=df_filtered.filter(col4)
     d=d.rename(columns={"prname": "province", "numtotal": "total cases"})
     d=d.set_index("province")
     d.to_json (r'total.json') # file for total death cases
+    uploadDataOnGit('total.json')
 
     d=df_filtered.filter(col5)
     d=d.rename(columns={"prname": "province", "percentactive": "active percentage"})
     d=d.set_index("province")
     d.to_json (r'active.json') # file for active percentage cases
-   
-
-    data = open("Death.json", "r").read()
-
-    token = "385b412e10720f3ea07bef43cf810bde19e1d828"
-    owner = 'khushbupatel24'
-    repo = 'covid-data'
-    path = 'deathRate.json'
-
-    url = 'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
-    r = requests.get(f'https://api.github.com/repos/{owner}/{repo}/contents/{path}')
-    print(r.json()['sha'])
-
-    r = requests.put(
-        f'https://api.github.com/repos/{owner}/{repo}/contents/{path}',
-        headers = {
-            'Authorization': f'Token {token}'
-        },
-        json = {
-            "message": "add new file",
-            "content": base64.b64encode(data.encode()).decode(),
-            "branch": "main",
-            "sha": r.json()['sha']
-        }
-    )
-    print(r.status_code)
-    print(r.json())
+    uploadDataOnGit('active.json')
 
 filter()
 
