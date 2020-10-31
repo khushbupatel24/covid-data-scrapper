25df44abf8e94d06b83b82766fdb82e0996ec3d0
amrit1203 <asbenipal24@gmail.com>
2020-10-31 14:01:33 +0530

    register API

---
+++ data/register.py
@@ -0,0 +1,46 @@
+from cryptography.fernet import Fernet
+import mysql.connector
+from mysql.connector import Error
+from mysql.connector import errorcode
+
+db_name = "covid"
+db_host = "localhost"
+db_password = ""
+db_user = "root"
+ 
+try:
+    connection = mysql.connector.connect(host= db_host,database= db_name,user= db_user,password= db_password)
+
+    m="my deepest secret"
+    e=m.encode()
+    key = Fernet.generate_key()
+    f=Fernet(key)
+    safe = f.encrypt(e)
+    #f.decrypt(safe)
+
+    query = """INSERT INTO user(name,email,password,ID) VALUES ('adsdsasa', 'sdasdsadsad','aasasa', 2200) """
+
+    cursor = connection.cursor()
+    cursor.execute(query)
+
+    connection.commit()
+    print(cursor.rowcount, "Registration successfull")
+    cursor.close()
+
+except mysql.connector.Error as error:
+    print("Failed to signup".format(error))
+
+finally:
+    if (connection.is_connected()):
+        connection.close()
+        print("Connection is closed")
+
+
+
+
+
+
+
+
+
+
