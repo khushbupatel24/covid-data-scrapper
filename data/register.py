3764d325c07e01b980af06a6400f68686e34f62d
amrit1203 <asbenipal24@gmail.com>
2020-10-31 14:33:37 +0530

    user function and insert into db

--- data/register.py
+++ data/register.py
@@ -7,33 +7,42 @@ db_name = "covid"
 db_host = "localhost"
 db_password = ""
 db_user = "root"
- 
-try:
-    connection = mysql.connector.connect(host= db_host,database= db_name,user= db_user,password= db_password)
 
-    m="my deepest secret"
-    e=m.encode()
-    key = Fernet.generate_key()
-    f=Fernet(key)
-    safe = f.encrypt(e)
-    #f.decrypt(safe)
+def user():
+    n="amrit"
+    e="abd@gmail.com"
+    d=2132132
+    p="kjdsakdjsk"
+    insert(n,e,p,d)
 
-    query = """INSERT INTO user(name,email,password,ID) VALUES ('adsdsasa', 'sdasdsadsad','aasasa', 2200) """
+def insert(n,e,p,id):
+        try:
+            connection = mysql.connector.connect(host= db_host,database= db_name,user= db_user,password= db_password)
+            cursor = connection.cursor()
+           
+            e=p.encode()
+            key = Fernet.generate_key()
+            f=Fernet(key)
+            safe = f.encrypt(e)
+            
+            query = """INSERT INTO user(name,email,password,ID) VALUES (%s, %s, %s, %s) """
+            record = (n, e, safe, id)
+            cursor = connection.cursor()
+            cursor.execute(query,record)
 
-    cursor = connection.cursor()
-    cursor.execute(query)
+            connection.commit()
+            print(cursor.rowcount, "Registration successfull")
+            cursor.close()
 
-    connection.commit()
-    print(cursor.rowcount, "Registration successfull")
-    cursor.close()
+        except mysql.connector.Error as error:
+            print("Failed to signup".format(error))
 
-except mysql.connector.Error as error:
-    print("Failed to signup".format(error))
+        finally:
+            if (connection.is_connected()):
+                connection.close()
+                print("Connection is closed")
 
-finally:
-    if (connection.is_connected()):
-        connection.close()
-        print("Connection is closed")
+user()
 
 
 
